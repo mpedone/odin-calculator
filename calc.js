c9a7de628e7cdb2d1783e11e0d13eb9a7318bb6a
Matthew Pedone <12798189+mpedone@users.noreply.github.com>
2024-09-30 11:18:10 -0400

    Add code to manipulate display
    I first added code to dynamically change the display, then the clear
    function, so that pressing the "C" button would revert the display to 0.
    Then, I added a class to each number, selected them all, and added an
    event listener to pass each value to the display, though only one at a
    time.

--- calc.js
+++ calc.js
@@ -22,3 +22,16 @@ function operate(a, b, operator){
     result = operator(a,b)
     alert(result)
 }
+
+const displayWindow = document.querySelector(".display p");
+displayWindow.textContent = 0;
+
+const clear = document.querySelector(".clear");
+const numerals = document.querySelectorAll(".numeral");
+
+clear.addEventListener('click', () => 
+    displayWindow.textContent = 0);
+
+numerals.forEach(numeral => 
+    numeral.addEventListener('click', () => 
+        displayWindow.textContent = numeral.textContent))
