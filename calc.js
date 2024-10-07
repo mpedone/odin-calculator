b6d24b5b65ebb3856492d6d7fb465cf78f41ae16
Matthew Pedone <12798189+mpedone@users.noreply.github.com>
2024-10-07 10:53:11 -0400

    Code single-digit arithemtic functions
    The calculator will now display and perform single-digit calculations.
    This is not a valid solution, but merely the first step. The current
    functionality relies on a user clicking buttons in a very specific
    order. They MUST click a number, an operator, a number, and then...
    Well, ideally the equals sign, but anything at all will trigger the
    arithemtic.
    
    I used a simple counter (count) to keep track of which input is which.
    The first input (count = 1) is the first number. The second input
    (count = 2) is the operator, the third input (count = 3) is the
    second number, and the fourth input (count = 4) is the arithemtic
    function call, and then the counter is reset to 0.
    
    This does not take into account the user clicking anything other than
    what is expected each time. It also does not allow the user to enter
    multi-digit numbers, like "11", or anything with a decimal point. It
    does not account for division by 0. These are all things I must figure
    out going forward.

--- calc.js
+++ calc.js
@@ -20,18 +20,76 @@ function divide (a,b){
 
 function operate(a, b, operator){
     result = operator(a,b)
-    alert(result)
+    // alert(result)
+    return result;
 }
 
 const displayWindow = document.querySelector(".display p");
 displayWindow.textContent = 0;
 
 const clear = document.querySelector(".clear");
-const numerals = document.querySelectorAll(".numeral");
+const buttons = document.querySelectorAll(".button");
+// const plus = document.querySelector(".add");
+// const minus = document.querySelector(".subtract");
+// const times = document.querySelector(".multiply");
+// const quotient = document.querySelector(".divide");
+// const decimal = document.querySelector(".decimal");
+// const equals = document.querySelector(".equals");
+// const numerals = document.querySelectorAll(".numeral");
 
 clear.addEventListener('click', () => 
     displayWindow.textContent = 0);
 
-numerals.forEach(numeral => 
-    numeral.addEventListener('click', () => 
-        displayWindow.textContent = numeral.textContent))
+let displayText = [];
+let entry;
+let numerals = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.']
+let operators = {'add': '+', 'subtract': '-', 'multiply': '*', 'divide': '/', 'equals': '='}
+
+
+// buttons.forEach(button => 
+//     button.addEventListener('click', (event) => {
+//         let target = event.target;
+//         let value = target.id
+//         console.log(value);
+//         if (numerals.includes(value)) {
+//             displayText.push(value);
+//             displayWindow.textContent = displayText.join('');
+//         } else if (operators.includes(value)) {
+//             firstNumber = parseFloat(displayWindow.textContent);
+//             displayWindow.textContent = 0;
+//             operator = value;
+//         }
+//         // console.log(displayText.join(''));
+//         console.log(firstNumber + " " + operator);
+//     })
+// )
+
+let count = 0;
+
+buttons.forEach(button => 
+    button.addEventListener('click', (event) => {
+        displayWindow.textContent = '';
+        count += 1;
+        let target = event.target;
+        let value = target.id;
+        if (count === 1) {
+            firstNumber = parseInt(value);
+            displayWindow.textContent = '';
+            displayWindow.textContent = firstNumber;
+        } else if (count === 2) {
+            operator = value;
+            displayWindow.textContent = '';
+            displayWindow.textContent = operators[operator];
+        } else if (count === 3) {
+            secondNumber = parseInt(value);
+            displayWindow.textContent = '';
+            displayWindow.textContent = secondNumber;
+        } else {
+            result = operate(firstNumber, secondNumber, window[operator]);
+            displayWindow.textContent = '';
+            displayWindow.textContent = result;
+            count = 0;
+        }
+
+    })
+)
