294c79cf52b9c45fecc99ed36aa018d8ea17c5fe
Matthew Pedone <12798189+mpedone@users.noreply.github.com>
2024-10-12 14:03:20 -0400

    Edit code to allow for multi-digit numbers
    I initially had all of the keys classed as "button", then was listening
    for any of them to be clicked, and trying to differentiate which was
    which. It was kind of working, but with a ton of nested if-else
    statements to try to account for every scenario. I went in and changed
    the numbers to class "numeral" and the operators to class "operator",
    and was then able to listen JUST for either of them. I kept the counter
    variable, as that lets the script know which entry is being stored.
    When counter = 1, it's the first entry, when counter = 2, it's the
    second entry. I added logic to account for when to call operate(); if
    there's no secondNumber, we just store the operator. If secondNumber
    exists, then we call the function, the result becomes firstNumber,
    and secondNumber is cleared. This way, the next operator selected will
    wait for another number to be entered and a new operator.
    
    I still have a couple of things to change. If an operator is pressed
    before a number, then 0 should be passed to firstNumber. This should be
    as simple as adding an if statement to the operator listener.
    
    There's also the matter of repeated functions - if the equals key is
    pressed, the operate() function returns the answer, eg x + y, but if
    the equals key is pressed again, the function should be called again,
    this time as (x + y) + y.
    
    And, of course, I need to get the % and +/- keys working. I have
    functions written for them, it's just a matter of getting them to work.

--- calc.js
+++ calc.js
@@ -1,5 +1,5 @@
-let firstNumber;
-let secondNumber;
+let firstNumber = '';
+let secondNumber = '';
 let operator;
 
 function add (a,b){
@@ -19,77 +19,158 @@ function divide (a,b){
 }
 
 function operate(a, b, operator){
-    result = operator(a,b)
-    // alert(result)
+    // result = window[operator](parseFloat(a),parseFloat(b));
+    result = operator(parseFloat(a),parseFloat(b));
     return result;
+}
+
+function resetDisplay() {
+    displayWindow.textContent = 0;
+    firstNumber = '';
+    secondNumber = '';
+    operator = '';
+    counter = 1;
+}
+
+function toPercent(val) {
+    // return parseFloat(val) * .01;
+    return '0.0' + val;
+}
+
+function negate(val) {
+    // return parseFloat(val) * -1;
+    return '-' + val;
 }
 
 const displayWindow = document.querySelector(".display p");
 displayWindow.textContent = 0;
 
 const clear = document.querySelector(".clear");
-const buttons = document.querySelectorAll(".button");
-// const plus = document.querySelector(".add");
-// const minus = document.querySelector(".subtract");
-// const times = document.querySelector(".multiply");
-// const quotient = document.querySelector(".divide");
-// const decimal = document.querySelector(".decimal");
-// const equals = document.querySelector(".equals");
-// const numerals = document.querySelectorAll(".numeral");
+clear.addEventListener('click', resetDisplay);
 
-clear.addEventListener('click', () => 
-    displayWindow.textContent = 0);
-
+const numerals = document.querySelectorAll(".numeral");
+const operators = document.querySelectorAll(".operator");
+const plusMinus = document.querySelector("#plus-minus");
+const percent = document.querySelector("#percent");
 let displayText = [];
-let entry;
-let numerals = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.']
-let operators = {'add': '+', 'subtract': '-', 'multiply': '*', 'divide': '/', 'equals': '='}
+let entry = '';
+let counter = 1;
+
+// percent.addEventListener('click', toPercent(displayText))
+// plusMinus.addEventListener('click', negate(displayText))
+
+numerals.forEach(button => 
+    button.addEventListener('click', event => {
+        if (operator === 'equals') resetDisplay();
+        displayWindow.textContent = '';
+        let target = event.target;
+        let value = target.id;
+        if (counter === 1) {
+            firstNumber += value;
+            console.log(`1st: ${firstNumber}`);
+            displayWindow.textContent = firstNumber;
+        } else {
+            secondNumber += value;
+            console.log(`2nd: ${secondNumber}`);
+            displayWindow.textContent = secondNumber;
+        }
+    })
+)
+
+operators.forEach(op => 
+    op.addEventListener('click', event => {
+        let target = event.target;
+        let value = target.id;
+        if (!secondNumber) {
+            operator = value;
+            console.log(operator);
+            counter = 2;
+        } else if (secondNumber) {
+            let displayValue = operate(firstNumber, secondNumber, window[operator])
+            displayWindow.textContent = displayValue;
+            operator = value;
+            console.log(operator)
+            firstNumber = displayValue;
+            secondNumber = '';
+        }
+    })
+)
+
+
+
+
+
+const buttons = document.querySelectorAll(".button");
+
+
+// let numerals = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
+// let operators = {'add': '+', 'subtract': '-', 'multiply': '*', 'divide': '/', 'equals': '='};
+
+// let counter = 1;
+
+// buttons.forEach(button => 
+//     button.addEventListener('click', (event) => {
+//         if (operator === 'equals') {
+//             resetDisplay();
+//         }
+//         displayWindow.textContent = '';
+//         let target = event.target;
+//         let value = target.id;
+//         if (numerals.includes(value) && counter === 1) {
+//             firstNumber += value;
+//             displayWindow.textContent = firstNumber;
+//             console.log(`first: ${firstNumber}`);
+//         } else if (Object.keys(operators).includes(value) && firstNumber && !secondNumber) {
+//             displayWindow.textContent = firstNumber;
+//             operator = value;
+//             console.log(operator);
+//             counter = 2;
+//         } else if (Object.keys(operators).includes(value) && !firstNumber) {
+//             firstNumber = '0';
+//             displayWindow.textContent = firstNumber;
+//             operator = value;
+//             counter = 2;
+//         } else if (numerals.includes(value) && firstNumber) {
+//             secondNumber += value;
+//             displayWindow.textContent = secondNumber;
+//             console.log(`second: ${secondNumber}`);
+//         } else if (Object.keys(operators).includes(value) && firstNumber && secondNumber) {
+//             console.log("eval time!");
+//             let displayValue = operate(firstNumber, secondNumber, operator)
+//             console.log(displayValue);
+//             displayWindow.textContent = displayValue;
+//             firstNumber = displayValue;
+//             secondNumber = ''
+//             operator = value;
+//         }
+//     })
+// )
 
 
 // buttons.forEach(button => 
 //     button.addEventListener('click', (event) => {
+//         displayWindow.textContent = '';
+//         count += 1;
 //         let target = event.target;
-//         let value = target.id
-//         console.log(value);
-//         if (numerals.includes(value)) {
-//             displayText.push(value);
-//             displayWindow.textContent = displayText.join('');
-//         } else if (operators.includes(value)) {
-//             firstNumber = parseFloat(displayWindow.textContent);
-//             displayWindow.textContent = 0;
+//         let value = target.id;
+//         console.log(numerals.includes(value));
+//         if (count === 1) {
+//             firstNumber = value;
+//             displayWindow.textContent = '';
+//             displayWindow.textContent = firstNumber;
+//         } else if (count === 2) {
 //             operator = value;
+//             displayWindow.textContent = '';
+//             displayWindow.textContent = operators[operator];
+//         } else if (count === 3) {
+//             secondNumber = value;
+//             displayWindow.textContent = '';
+//             displayWindow.textContent = secondNumber;
+//         } else {
+//             result = operate(firstNumber, secondNumber, operator);
+//             displayWindow.textContent = '';
+//             displayWindow.textContent = result;
+//             count = 0;
 //         }
-//         // console.log(displayText.join(''));
-//         console.log(firstNumber + " " + operator);
 //     })
 // )
-
-let count = 0;
-
-buttons.forEach(button => 
-    button.addEventListener('click', (event) => {
-        displayWindow.textContent = '';
-        count += 1;
-        let target = event.target;
-        let value = target.id;
-        if (count === 1) {
-            firstNumber = parseInt(value);
-            displayWindow.textContent = '';
-            displayWindow.textContent = firstNumber;
-        } else if (count === 2) {
-            operator = value;
-            displayWindow.textContent = '';
-            displayWindow.textContent = operators[operator];
-        } else if (count === 3) {
-            secondNumber = parseInt(value);
-            displayWindow.textContent = '';
-            displayWindow.textContent = secondNumber;
-        } else {
-            result = operate(firstNumber, secondNumber, window[operator]);
-            displayWindow.textContent = '';
-            displayWindow.textContent = result;
-            count = 0;
-        }
-
-    })
-)
